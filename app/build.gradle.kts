5622406109d26384127ced6c0137076609bf6158
mcgregr22 <grejoe045@gmail.com>
2025-12-12 01:12:46 +0700

    progrress malam

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
